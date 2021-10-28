2cafa30ef7a31e48ffbea7ad8489d85378c08bfa
Jakub Dmochowski <jakub@dmochowski.co>
2021-10-28 15:00:41 +0200

    getBestPoint helper

--- src/py/Evolution.py
+++ src/py/Evolution.py
@@ -70,6 +70,9 @@ class Evolution:
     def getCentralPoint(population):
         return np.mean(population, axis=0)
 
+    def getBestPoint(self, population):
+        return population[np.argmax(np.array(list(map(lambda point: self.calculate(point), population))))]
+
     def init_population(self):
         size = (self.N, self.D)
         self.H = np.empty(0)

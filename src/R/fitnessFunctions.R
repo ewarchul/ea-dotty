912c5e85473bc8c48ecd0c7472ddd75f8000737e
Jakub Dmochowski <jakub@dmochowski.co>
2021-11-30 19:07:28 +0100

    bugfix

--- src/R/fitnessFunctions.R
+++ src/R/fitnessFunctions.R
@@ -2,26 +2,28 @@ library(dplyr)
 library(tidyverse)
 
 quadratic <- function(population) {
-  as_tibble(t(as_tibble(t(population)) %>% summarise(across(all_of(names(.)), ~ sum(.x*.x)))))
+  fun_raw = ~ sum(.x*.x)
+  as_tibble(t(as_tibble(t(population)) %>% summarise(across(all_of(names(.)), fun_raw))))
 }
 
 eliptic <- function(population, A) {
   D = nrow(population)
-  fun = ~ .x * .x * A^(D-.y)
-  as_tibble(t(as_tibble(t(population)) %>% summarise(across(all_of(names(.)), ~imap_dbl(.x,fun)))))
+  fun_raw = ~ .x * .x * A^(D-.y)
+  as_tibble(t(as_tibble(t(population)) %>% summarise(across(all_of(names(.)), ~sum(imap_dbl(.x,fun_raw))))))
 }
 
 rastrigin <- function(population, A) {
-  fun = ~ .x * .x - A * cos(2 * pi * .x)
-  as_tibble(t(as_tibble(t(population)) %>% summarise(across(all_of(names(.)), ~imap_dbl(.x,fun)))))
+  fun_raw = ~ .x * .x - A * cos(2 * pi * .x)
+  as_tibble(t(as_tibble(t(population)) %>% summarise(across(all_of(names(.)), ~sum(imap_dbl(.x,fun_raw))))))
 }
 
 rosenbrock <- function(population) {
-  fun = function(point) {
+  fun_raw = function(point) {
     d=length(point)
     xi = point[1:(d-1)]
     xnext = point[2:d]
     sum(100*(xnext-xi^2)^2 + (xi-1)^2)
   }
-  as_tibble(t(as_tibble(t(population)) %>% summarise(across(all_of(names(.)), ~fun(.x)))))
+  as_tibble(t(as_tibble(t(population)) %>% summarise(across(all_of(names(.)), ~sum(fun_raw(.x))))))
 }
+

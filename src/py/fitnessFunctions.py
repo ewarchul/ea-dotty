e98ca6e5f805a1f8343561af77d8e5e2dc0e53a7
Wojciech Sitek <s.wojciech9@gmail.com>
2022-01-01 14:21:36 +0100

    De/rand/1/bin, sade, jade, lshade with flow implementation in algo.ipynb

--- src/py/fitnessFunctions.py
+++ src/py/fitnessFunctions.py
@@ -2,33 +2,48 @@ from functools import reduce
 from math import pi, cos, exp, sqrt
 
 
-def spherical(population):
-    return list(map(lambda point: reduce(lambda cum, x: cum + x * x, point, 0), population))
+def spherical_pop(population):
+    return list(map(spherical, population))
 
 
-def elliptic(population):
-    D = len(population[0])
-    return list(map(lambda point:
-                    reduce(lambda cum, x: (cum[0] + pow(10, 6 * (cum[1] - 1) / (D - 1)) * x * x, cum[1] + 1), point,
-                           (0, 1))[0], population))
+def elliptic_pop(population):
+    return list(map(elliptic, population))
 
 
-def rastrigin(population):
-    return list(map(lambda point: reduce(lambda cum, x: cum + x * x - 10 * cos(2 * pi * x) + 10, point, 0), population))
+def rastrigin_pop(population):
+    return list(map(rastrigin, population))
 
 
-def rosenbrock(population):
-    D = len(population[0])
-    return list(
-        map(lambda point: reduce(lambda cum, p: (lambda x, y: cum + 100 * pow(x * x - y, 2) + pow(x - 1, 2))(*p),
-                                 zip(point[:D - 1], point[1:]), 0), population))
+def rosenbrock_pop(population):
+    return list(map(rosenbrock, population))
 
 
-def ackley(population):
-    def map_point(point):
-        quad = spherical(point)
-        cosfunc = reduce(lambda cum, x: cum + cos(2 * pi * x), point, 0)
-        D = len(population)
-        return reduce(lambda cum, x: cum - 20 * exp(-0.2 * sqrt(quad / D)) - exp(cosfunc / D) + 20 + exp(1), point, 0)
+def ackley_pop(population):
+    return list(map(ackley, population))
 
-    return list(map(map_point, population))
+
+def spherical(point):
+    return reduce(lambda cum, x: cum + x * x, point, 0)
+
+
+def elliptic(point):
+    D = len(point)
+    return reduce(lambda cum, x: (cum[0] + pow(10, 6 * (cum[1] - 1) / (D - 1)) * x * x, cum[1] + 1), point,
+                  (0, 1))[0]
+
+
+def rastrigin(point):
+    return reduce(lambda cum, x: cum + x * x - 10 * cos(2 * pi * x) + 10, point, 0)
+
+
+def rosenbrock(point):
+    D = len(point)
+    return reduce(lambda cum, p: (lambda x, y: cum + 100 * pow(x * x - y, 2) + pow(x - 1, 2))(*p),
+                  zip(point[:D - 1], point[1:]), 0)
+
+
+def ackley(point):
+    quad = spherical(point)
+    cosfunc = reduce(lambda cum, x: cum + cos(2 * pi * x), point, 0)
+    D = len(point)
+    return reduce(lambda cum, x: cum - 20 * exp(-0.2 * sqrt(quad / D)) - exp(cosfunc / D) + 20 + exp(1), point, 0)

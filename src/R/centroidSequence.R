49df201e73e5dbf8d5b166b0d6230e16abdea1cc
Jakub Dmochowski <jakub@dmochowski.co>
2022-01-11 14:04:21 +0100

    bugfix: reverted sequence

--- src/R/centroidSequence.R
+++ src/R/centroidSequence.R
@@ -22,10 +22,11 @@ getSequence <- function(population, fitfunc) {
   P <- population  %>% rowwise() %>% mutate(f = fitfunc(c_across()))
   P <- P[order(P$f), ]
   P <- select(P, -f)
-  reduce(as.data.frame(t(P)), function(cum, x){
+  seq_rev = reduce(as.data.frame(t(P)), function(cum, x){
     tmp_population <- rbind(cum[[1]], x)
     central_point <- getCentralPoint(tmp_population)
     tmp_estimators <- rbind(cum[[2]], central_point)
     list(tmp_population, tmp_estimators)
   }, .init=list(data.frame(), data.frame()))[[2]]
+  seq_rev[nrow(seq_rev):1, ]
 }
